00bab7eb40c40c1b5bc069412f64122ffa590deb
RainbowBird <git@luoling.moe>
2025-01-18 19:08:07 +0800

    style: import

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,4 +2,17 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   formatters: true,
+  rules: {
+    'import/order': [
+      'error',
+      {
+        'groups': [
+          ['type'],
+          ['builtin', 'external'],
+          ['parent', 'sibling', 'index'],
+        ],
+        'newlines-between': 'always',
+      },
+    ],
+  },
 })
